dadb82cbcc148beb7cc4644ad469b76a6376e304
Roberto Marquez <onebeartoe@gmail.com>
2018-11-15 07:17:31 -0600

    The copy functionality was implemented.

--- src/main/ansible/roles/octoprint/email-notifications/configuration/Configuration.groovy
+++ src/main/ansible/roles/octoprint/email-notifications/configuration/Configuration.groovy
@@ -1,26 +1,41 @@
 
 import java.io.File;
-
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.nio.file.Path;
+    
 evaluate( new File("ConfigurationPaths.groovy") )
 
-def filePath = "~/.octoprint/config.yml"
+// Does the Events configuration already exists?
+def configContents = Files.readAllBytes( Path.get(configFilePath))
 
-def file = new File(filePath)
+def configExists = configContents.contains("events:") && configContents.contains("subscriptions:");
 
-if( file.exists() )
+if(configExists)
 {
-    println "file found"
+    println "the configuration already exists, no operation performed."
 }
 else
 {
-    println "file NOT found"
-}
-
-if( file.canRead() )
-{
-    println "the file is readable"
-}
-else
-{
-    println "file CANNOT be read"
+    // Does the backup file exist?
+    def backupFile = new File(backupFilePath);
+    if( backupFile.exists() )
+    {
+        println "The backup file already exists, skipping backup of configuration"
+    }
+    else
+    {
+        // backup the configuration file in case a revert is needed
+        def source = Path.get(configFilePath);
+        
+        def outstream = FileOutputStream(backupFile);
+        
+        Files.copy(source, outstream);        
+    }
+    
+    // append the 'events:' block to the configuration file
+    def outpath = Path.get(configFilePath);
+    def text = Files.readAllBytes("config.xml.events")
+    Files.write(outpath, text, StandardOpenOption.APPEND)
 }
