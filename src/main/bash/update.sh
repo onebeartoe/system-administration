d3c9e653b44bd9414646e48b5a02b0c1b120146c
Roberto Marquez <onebeartoe@gmail.com>
2019-11-22 10:43:29 -0600

    add a BASH script to update JARs under the /opt/onebeartoe/ directory

---
+++ src/main/bash/update.sh
@@ -0,0 +1,22 @@
+
+export filesystemFindette=~/Versioning/owner/github/onebeartoe/productivity/filesystem/target/filesystem-findette.jar
+export filesystemMovette=~/Versioning/owner/github/onebeartoe/productivity/filesystem/target/filesystem-movette.jar
+export filesystemPopulater=~/Versioning/owner/github/onebeartoe/software-development-tools/filesystem/target/filesystem-1.0-SNAPSHOT-jar-with-dependencies.jar
+export htmlUtility=~/Versioning/owner/github/onebeartoe/software-development-tools/html-utility/target/html-utility-1.0-SNAPSHOT-jar-with-dependencies.jar
+export imageResizer=~/Versioning/owner/github/onebeartoe/imaging/resizer/target/resizer-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+export openscadInliner=~/Versioning/owner/github/onebeartoe/3D-Modeling/openscad/inline-tool/target/openscad-inliner-0.0.1-SNAPSHOT.jar
+export openscadTestSuite=~/Versioning/owner/github/onebeartoe/3D-Modeling/openscad/test-suite/target/test-suite-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+export versionControl=~/Versioning/owner/github/onebeartoe/software-development-tools/version-control/target/version-control-1.0-SNAPSHOT-jar-with-dependencies.jar
+export timer=~/Versioning/owner/github/onebeartoe/chronos/timer/target/timer-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+
+export optDir=/opt/onebeartoe/
+
+cp $filesystemFindette $optDir
+cp $filesystemMovette $optDir
+cp $filesystemPopulater $optDir
+cp $htmlUtility $optDir
+cp $imageResizer $optDir
+cp $openscadInliner $optDir
+cp $openscadTestSuite $optDir
+cp $versionControl $optDir
+cp $timer $optDir
