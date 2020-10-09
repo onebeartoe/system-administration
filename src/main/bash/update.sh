37fa28c593c60d0da8ecf8cdd60545e9aa4acdcb
Roberto Marquez <onebeartoe@gmail.com>
2020-10-09 16:37:41 -0600

    add shortcuts

--- src/main/bash/update.sh
+++ src/main/bash/update.sh
@@ -1,7 +1,12 @@
 
+
+export onebeartoePath='/home/roberto/Versioning/owner/github/onebeartoe'
+
+
 export filesystemFindette=~/Versioning/owner/github/onebeartoe/productivity/filesystem/target/filesystem-findette.jar
 export filesystemMovette=~/Versioning/owner/github/onebeartoe/productivity/filesystem/target/filesystem-movette.jar
 export filesystemPopulater=~/Versioning/owner/github/onebeartoe/software-development-tools/filesystem/target/filesystem-1.0-SNAPSHOT-jar-with-dependencies.jar
+export filesytemWatcher="$onebeartoePath/software-development-tools/filesystem/watcher/command-line/target/onebeartoe-filesystem-cli-watcher-cli-jar-with-dependencies.jar"
 export htmlUtility=~/Versioning/owner/github/onebeartoe/software-development-tools/html-utility/target/html-utility-1.0-SNAPSHOT-jar-with-dependencies.jar
 export imageResizer=~/Versioning/owner/github/onebeartoe/imaging/resizer/target/resizer-0.0.1-SNAPSHOT-jar-with-dependencies.jar
 export openscadInliner=~/Versioning/owner/github/onebeartoe/3D-Modeling/openscad/inline-tool/target/openscad-inliner-0.0.1-SNAPSHOT.jar
@@ -14,6 +19,7 @@ export optDir=/opt/onebeartoe/
 cp $filesystemFindette $optDir
 cp $filesystemMovette $optDir
 cp $filesystemPopulater $optDir
+cp $filesytemWatcher $optDir
 cp $htmlUtility $optDir
 cp $imageResizer $optDir
 cp $openscadInliner $optDir
